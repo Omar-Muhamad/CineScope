4a3b39c3ebf9d1ace8876fda6fd90f4465e7aac1
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-04-30 12:15:47 +0300

    Add redux toolkit & customizable button ui component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,34 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { decrement, increment } from "../redux/counter/counterSlice";
+import Button from "../components/ui/Button";
+
 const Home = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div className="text-xl">Home</div>
-  )
-}
-export default Home
+    <>
+      <h1 className="text-xl">Home</h1>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+        <Button>Add me</Button>
+      </div>
+    </>
+  );
+};
+export default Home;
