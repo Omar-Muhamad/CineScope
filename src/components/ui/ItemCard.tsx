08348d874233e654c1d38de4c7c2798d0c9ed9b9
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-05-07 19:46:36 +0300

    Another fix to the layout shift

--- src/components/ui/ItemCard.tsx
+++ src/components/ui/ItemCard.tsx
@@ -65,8 +65,8 @@ const ItemCard: FC<ItemCardProps> = ({
           <Text>{ratings}</Text>
         </div>
         <NavLink to={media_type === "movie" ? `/movie/${id}` : `/tv/${id}`}>
-          <Heading as="h3" size="sm">
-            {title.length >= 23 ?`${title.slice(0, 22)}...`: title}
+          <Heading as="h3" size="sm" className="h-12">
+            {title}
           </Heading>
         </NavLink>
       </div>
